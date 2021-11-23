081d264c6022ef79ec1133e99439c36dadb1d94c
EmanuelJRod <emanuel.rodrig95@gmail.com>
2021-11-23 18:19:49 -0300

    Primer comit, subiendo el proyecto entero.

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+console.log("Funciona");
+window.addEventListener("load", function () {
+    //Captura de elementos HTML
+    const btnUnirse = this.document.querySelector("#botonUnirse");
+    btnUnirse.addEventListener("click", function () {
+        console.log("Se presiono le boton unirse");
+        swal({
+            title:"¡GRACIAS POR SUMARTE!",
+            text:"Para completar tu inscripción completá el formulario."
+        });
+    })
+})
